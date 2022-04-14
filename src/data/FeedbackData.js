ebe42090a79aebd349e657ead330d7bbb5a3f291
Patrick Nepp <patricknepp@Patricks-MBP.attlocal.net>
2022-04-13 19:23:30 -0500

    initial commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus quo consequuntur maiores odit ad mollitia, praesentium eveniet animi amet quis hic quaerat, dolores molestias velit voluptate modi est adipisci. Doloribus.'
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus quo consequuntur maiores odit ad mollitia, praesentium eveniet animi amet quis hic quaerat, dolores molestias velit voluptate modi est adipisci. Doloribus.'
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus quo consequuntur maiores odit ad mollitia, praesentium eveniet animi amet quis hic quaerat, dolores molestias velit voluptate modi est adipisci. Doloribus.'
+    },
+]
+
+export default FeedbackData;
